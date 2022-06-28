56637d42c31d28c40017ebdb8c7e5fcf396d5025
Estefania <estefania.osses.v@gmail.com>
2022-06-28 11:14:59 -0400

    style: add form for login from mui

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { signIn } from '../controller/api';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
