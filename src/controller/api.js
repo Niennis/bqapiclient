030739f4349f7efcdcb710d7ef309a1b86ebe27d
Estefania <estefania.osses.v@gmail.com>
2022-07-03 16:09:46 -0400

    feat: add getItemById function, and format url in fetch

--- src/controller/api.js
+++ src/controller/api.js
@@ -1,7 +1,8 @@
-const API_URL = 'https://bq-niennis.herokuapp.com';
+// const API_URL = 'https://bq-niennis.herokuapp.com';
+const API_URL = 'http://localhost:80';
 
 export const signIn = (route, user) => {
-  return fetch(`${API_URL}${route}`, {
+  return fetch(`${API_URL}/${route}`, {
     method: 'POST',
     body: JSON.stringify(user),
     headers: {
@@ -12,7 +13,8 @@ export const signIn = (route, user) => {
 }
 
 export const getItems = (route, authToken) => {
-  return fetch(`${API_URL}${route}`, {
+  // console.log(route, authToken)
+  return fetch(`${API_URL}/${route}`, {
     method: 'GET',
     headers: {
       "content-type": "application/json",
@@ -23,7 +25,30 @@ export const getItems = (route, authToken) => {
 }
 
 export const sendOrder = (route, body, authToken) => {
-  return fetch(`${API_URL}${route}`, {
+  return fetch(`${API_URL}/${route}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      "content-type": "application/json",
+      authorization: 'Bearer ' + authToken,
+    }
+  })
+    .then(response => response.json())
+}
+
+export const getItemById = (route, id, authToken) => {
+  return fetch(`${API_URL}/${route}/${id}`, {
+    method: 'GET',
+    headers: {
+      "content-type": "application/json",
+      authorization: 'Bearer ' + authToken,
+    }
+  })
+    .then(response => response.json())
+}
+
+export const newItem =  (route, body, authToken) => {
+  return fetch(`${API_URL}/${route}`, {
     method: 'POST',
     body: JSON.stringify(body),
     headers: {
@@ -35,8 +60,8 @@ export const sendOrder = (route, body, authToken) => {
 }
 
 export const updateItem = async (route, itemId, body, authToken) => {
-  const response = await fetch(`${API_URL}${route}${itemId}`, {
-    method: 'PATCH',
+  const response = await fetch(`${API_URL}/${route}/${itemId}`, {
+    method: 'PUT',
     body: JSON.stringify(body),
     headers: {
       "content-type": "application/json",
@@ -45,4 +70,16 @@ export const updateItem = async (route, itemId, body, authToken) => {
   });
   const resp = await response.json();
   return resp;
+}
+
+
+export const deleteItem = (route, id, authToken) => {
+  return fetch(`${API_URL}/${route}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      "content-type": "application/json",
+      authorization: 'Bearer ' + authToken,
+    }
+  })
+    .then(response => response.json())
 }
