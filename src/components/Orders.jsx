93701d7ddb73a75af91b3203743c824a830c43d2
Estefania <estefania.osses.v@gmail.com>
2022-07-03 16:07:47 -0400

    style: set card component from mui to style order

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -4,40 +4,28 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-// import { flexbox } from '@mui/system';
-import { purple } from '@mui/material/colors';
+import AlertDialog from './Alert';
 
-export const Orders = ({ client, qtyProducts, product, handleUpdate }) => {
-
-  const ColorButton = styled(Button)(({ theme }) => ({
-    color: theme.palette.getContrastText(purple[100]),
-    backgroundColor: purple[100],
-    '&:hover': {
-      backgroundColor: purple[200],
-    },
-    spacing: 50,
-  }));
+export const Orders = ({ order, qtyProducts, getOrder, handleUpdate }) => {
 
   return (
-      <Card sx={{ maxWidth: 220, bgcolor: '#6a1b9a', width: 220, height: {qtyProducts} * 100, color: '#e1bee7' }}>
-        <CardActionArea>
+    <Card sx={{ maxWidth: 220, bgcolor: '#6a1b9a', width: 220, height: { qtyProducts } * 100, color: '#e1bee7' }}>
+      <CardActionArea>
 
-          <CardContent sx={{ padding: 0, margin: 0 }}>
-            <Typography gutterBottom component="div" sx={{ padding: 0, margin: 0, fontSize: 16 }}>
-              {client}
-            </Typography>
-            {product.products.map(prod => {
-              return (
-                  <Typography key={product.id + prod.product.id} variant="body2" color="text.secondary" sx={{color: '#e1bee7'}}>
-                    {prod.qty} {prod.product.name}
-                  </Typography>
-              )
-            })}
-            <ColorButton variant="contained" sx={{m:1}} onClick={handleUpdate}>Entregar</ColorButton>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+        <CardContent sx={{ padding: 0, margin: 0 }}>
+          <Typography gutterBottom component="div" sx={{ padding: 0, margin: 0, fontSize: 16 }}>
+            {order.client}
+          </Typography>
+          {order.products.map(prod => {
+            return (
+              <Typography key={order.id + prod.product.id} variant="body2" color="text.secondary" sx={{ color: '#e1bee7' }}>
+                {prod.qty} {prod.product.name}
+              </Typography>
+            )
+          })}
+        </CardContent>
+      </CardActionArea>
+          <AlertDialog action={'Entregar'} handleGet={getOrder} handleUpdate={handleUpdate} />
+    </Card>
   )
 }
