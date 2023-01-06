e90c4ff83ba4d8e0cf5e5c861d438d9b703d8f01
Estefania <estefania.osses.v@gmail.com>
2023-01-06 01:41:21 -0300

    Updates

--- src/components/SendButton.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { ColorButton } from '../utils/theme';
-
-export default function CustomizedButtons({ sendOrder }) {
-  return (
-    <ColorButton variant="contained" onClick={sendOrder}>Enviar orden</ColorButton>
-  );
-}
